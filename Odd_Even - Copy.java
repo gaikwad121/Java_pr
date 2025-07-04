47dd6a5b80838f5089ba31dada81e8442708107a
Jyoti <jg.jyotigaikwad121@gmail.com>
2025-07-05 01:25:59 +0530

    Add

--- Odd_Even - Copy.java
+++ Odd_Even - Copy.java
@@ -4,9 +4,7 @@ public static void main(String[]args){
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter any number");
 int num=sc.nextInt();
-if(num%2==0){
-System.out.println("The Number is Even");
-}
-else{
-System.out.println("The Number is odd");
+for(int i=1;i<=num;i++){
+if(num%i==0){
+System.out.println(i);
 }}}
